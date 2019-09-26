da4ee18fbecd733ffd777bcc18c551bca476396b
FIC <FIC@info.local>
2019-09-26 15:20:25 -0300

    Versão 4

---
+++ Aprendendo Função/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Aprendendo_Função
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TemaAula());
+
+
+            Console.WriteLine("Digite o primeiro número");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int numero2 = int.Parse(Console.ReadLine());
+
+
+            int resultadoDaSoma = SomaDeDoisNumeros(numero1, numero2);
+            Console.WriteLine($"O resultado da soma é: {resultadoDaSoma}");
+
+
+           
+
+        }//fim do main
+
+        //funções
+
+        /// <sumary>Efetua a soma de dois números</sumery> 
+        /// <param name="a"> Primeiro valor inteiro</param>
+        /// <param name="b"> Segundo valor inteiro</param>
+        /// <returns> Retorna a soma dos dois números</returns>
+        public static int SomaDeDoisNumeros(int a, int b)
+        {
+            int soma = a + b;
+            return soma;
+        }
+
+        
+        /// <sumary>Efetua a soma de dois números</sumery> 
+        public static string TemaAula()
+        {
+            return "Aprendendo Função";
+        }
+    }
+}
---
+++ Aula POO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Aula_POO.Models;
+
+namespace Aula_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlunoModel aluno1 = new AlunoModel();
+
+            Console.Clear();
+
+            System.Console.Write("\nInsira o nome do aluno: ");
+            aluno1.Nome = Console.ReadLine();
+
+            System.Console.Write("\nInsira o curso do aluno: ");
+            aluno1.Curso = Console.ReadLine();
+
+            System.Console.Write("\nInsira o documento (RG) do aluno: ");
+            aluno1.Rg = int.Parse(Console.ReadLine());
+            
+            System.Console.Write("\nInsira a idade do aluno: ");
+            aluno1.Idade = int.Parse(Console.ReadLine());
+
+            System.Console.Write($@"
+            Nome: {aluno1.Nome}
+            Curso: {aluno1.Curso}
+            Documento: {aluno1.Rg}
+            Idade: {aluno1.Idade}
+            ");
+
+        }
+    }
+}
