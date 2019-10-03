9dbcedc61d2766cf8eb4a7b1d6a9ea8a0d9f67eb
Barbaraale <barbaraoliale@gamil.com>
2019-10-03 00:28:14 -0300

    Atualização 3

--- Atividade Vingadores/Controllers/JogoController.cs
+++ Atividade Vingadores/Controllers/JogoController.cs
@@ -14,94 +14,88 @@ namespace Atividade_Vingadores.Controllers
 
             CapitaoAmericaModel capitaoAmerica = new CapitaoAmericaModel();
             
+            public void CapitaoAmerica(){
 
+                string cor = "Azul";
 
+                capitaoAmerica.Equipe = equipe;
+                capitaoAmerica.Vida = vida;
+                capitaoAmerica.Cor = cor;
 
-
-        public void CapitaoAmerica(){
-
-            string cor = "Azul";
-
-            capitaoAmerica.Equipe = equipe;
-            capitaoAmerica.Vida = vida;
-            capitaoAmerica.Cor = cor;
-
-            Console.Write("\nO Capitão América está com escudo? (true ou false): ");
-            bool escudo = bool.Parse(Console.ReadLine());
+                Console.Write("\nO Capitão América está com escudo? (true ou false): ");
+                bool escudo = bool.Parse(Console.ReadLine());
             
-            if (escudo)
-            {
-                Console.WriteLine("\nEle está com o escudo!");
-                Console.WriteLine("\n-------------------------------------------------------------");
-            }else{
-                Console.WriteLine("\nEle não está com escudo!");
-                Console.WriteLine("\n-------------------------------------------------------------");
+                if (escudo)
+                {
+                    Console.WriteLine("\nEle está com o escudo!");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                }else{
+                    Console.WriteLine("\nEle não está com escudo!");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                }
+
+                capitaoAmerica.Escudo = escudo;
+        
             }
 
-            capitaoAmerica.Escudo = escudo;
+            public void ListaCapitãoAmérica(){
+
+                // System.Console.Write("\nListar informações do personagem? (true ou false): ");
+                // bool lista = bool.Parse(Console.ReadLine());
+
+                // if (lista)
+                // {
+                
+                    Console.WriteLine("\nInformações do Personagem\n");
+                    Console.WriteLine("Capitão América:");
+                    Console.WriteLine($"Equipe: {capitaoAmerica.Equipe}");
+                    Console.WriteLine($"Vida: {capitaoAmerica.Vida}");
+                    Console.WriteLine($"Escudo: {capitaoAmerica.Escudo}");
+                    Console.WriteLine($"Cor: {capitaoAmerica.Cor}");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+
+                // }else{
+
+                // }
+            
+            }
+
+
+
+            public void HomemFerro(){
+
+                string cor = "Vermelho";
+
+                homemFerro.Equipe = equipe;
+                homemFerro.Vida = vida;
+                homemFerro.Cor = cor;
+            
+                Console.Write("\nO Homem de Ferro está com a armadura? (true ou false): ");
+                bool armadura = bool.Parse(Console.ReadLine());
+
+                if (armadura)
+                {
+                    Console.WriteLine("\nEle está com armadura!");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                }else{
+                    Console.WriteLine("\nEle não está com armadura!");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                }
+
+                homemFerro.Armadura = armadura;
+
+            }
         
-        }
-
-        public void ListaCapitãoAmérica(){
-
-            // System.Console.Write("\nListar informações do personagem? (true ou false): ");
-            // bool lista = bool.Parse(Console.ReadLine());
-
-            // if (lista)
-            // {
+            public void ListaHomemFerro(){
                 
                 Console.WriteLine("\nInformações do Personagem\n");
-                Console.WriteLine("Capitão América:");
-                Console.WriteLine($"Equipe: {capitaoAmerica.Equipe}");
-                Console.WriteLine($"Vida: {capitaoAmerica.Vida}");
-                Console.WriteLine($"Escudo: {capitaoAmerica.Escudo}");
-                Console.WriteLine($"Cor: {capitaoAmerica.Cor}");
+                Console.WriteLine("\nHomem de Ferro:");
+                Console.WriteLine($"Equipe: {homemFerro.Equipe}");
+                Console.WriteLine($"Vida: {homemFerro.Vida}");
+                Console.WriteLine($"Armadura: {homemFerro.Armadura}");
+                Console.WriteLine($"Cor: {homemFerro.Cor}");
                 Console.WriteLine("\n-------------------------------------------------------------");
 
-            // }else{
-
-            // }
-            
-        }
-
-
-
-
-
-        public void HomemFerro(){
-
-            string cor = "Vermelho";
-
-            homemFerro.Equipe = equipe;
-            homemFerro.Vida = vida;
-            homemFerro.Cor = cor;
-            
-            Console.Write("\nO Homem de Ferro está com a armadura? (true ou false): ");
-            bool armadura = bool.Parse(Console.ReadLine());
-
-            if (armadura)
-            {
-                Console.WriteLine("\nEle está com armadura!");
-                Console.WriteLine("\n-------------------------------------------------------------");
-            }else{
-                Console.WriteLine("\nEle não está com armadura!");
-                Console.WriteLine("\n-------------------------------------------------------------");
             }
-
-            homemFerro.Armadura = armadura;
-
-        }
-        
-        public void ListaHomemFerro(){
-            
-            Console.WriteLine("\nInformações do Personagem\n");
-            Console.WriteLine("\nHomem de Ferro:");
-            Console.WriteLine($"Equipe: {homemFerro.Equipe}");
-            Console.WriteLine($"Vida: {homemFerro.Vida}");
-            Console.WriteLine($"Armadura: {homemFerro.Armadura}");
-            Console.WriteLine($"Cor: {homemFerro.Cor}");
-            Console.WriteLine("\n-------------------------------------------------------------");
-
-        }
     }
-}
+}   
